cf9aff139359d2ff3b0300399b4df66ce684f717
Ben Hiller <benhiller@gmail.com>
2010-03-06 03:25:35 -0600

    should work with multiple GPUs, need to test

--- main.c
+++ main.c
@@ -13,12 +13,12 @@ int runCL(int width, int height)
 {
 	cl_kernel  kernel;
 
-	cl_command_queue cmd_queue;
+	cl_command_queue cmd_queue[16];
 	cl_context       context;
 
 	cl_int err = 0;
-  cl_uint devices = 0;
-  cl_device_id device[16];
+  cl_uint num_devices = 0;
+  cl_device_id devices[16];
 
 	size_t returned_size = 0;
 
@@ -29,18 +29,23 @@ int runCL(int width, int height)
 
   char *host_image = (char *) malloc(buffer_size);
 
-	context = create_context(&devices);
+	context = create_context(&num_devices);
   print_debug_info(context);
 
   err = clGetContextInfo(context, CL_CONTEXT_DEVICES, sizeof(cl_device_id) * 16,
-                         &device, NULL);
+                         &devices, NULL);
 	assert(err == CL_SUCCESS);
 
-	cmd_queue = clCreateCommandQueue(context, device[0], 0, NULL);
+  int i;
+  for(i = 0; i < num_devices; i++) {
+    cmd_queue[i] = clCreateCommandQueue(context, devices[i], 0, &err);
+    assert(err == CL_SUCCESS);
+  }
 
   // Mark this write only, since the kernel does not have to read the image it
   // is writing. I am not sure if this has any performance benefit.
-	image	= clCreateBuffer(context, CL_MEM_WRITE_ONLY, buffer_size, NULL, NULL);
+	image	= clCreateBuffer(context, CL_MEM_WRITE_ONLY, buffer_size, NULL, &err);
+  assert(err == CL_SUCCESS);
 
 	// Load the program source from disk
 	const char *filename = "mandelbrot.cl";
@@ -48,26 +53,41 @@ int runCL(int width, int height)
 
   // Now setup the arguments to our kernel
   // In our case, we just need to give it a pointer to the image
-  err  = clSetKernelArg(kernel,  0, sizeof(cl_mem), &image);
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &image);
   assert(err == CL_SUCCESS);
 
   // Run the calculation by enqueuing it and forcing the
   // command queue to complete the task
+  // To support multiple compute devices, need to split this up
+  // among all of them. Easiest way to split up is block alloc.
   size_t global_work_size[2] = {width, height};
-  err = clEnqueueNDRangeKernel(cmd_queue, kernel, 2, NULL,
-                               global_work_size, NULL, 0, NULL, NULL);
-  assert(err == CL_SUCCESS);
+  // Assuming that num_devices divides width and height evenly
+  size_t device_work_size[2] = {width, height/num_devices};
+  for(i = 0; i < num_devices; i++) {
+    size_t device_work_offset[2] = {0, device_work_size[1]*i};
+    size_t offset = device_work_offset[1]*3*width;
+    err = clEnqueueNDRangeKernel(cmd_queue[i], kernel, 2, device_work_offset,
+                                 device_work_size, NULL, 0, NULL, NULL);
+    assert(err == CL_SUCCESS);
 
-  err = clEnqueueReadBuffer(cmd_queue, image, CL_TRUE, 0, buffer_size,
-                            host_image, 0, NULL, NULL);
-  assert(err == CL_SUCCESS);
+    // Non-blocking read, so we can continue queuing up more kernels
+    err = clEnqueueReadBuffer(cmd_queue[i], image, CL_FALSE, offset,
+                              buffer_size/num_devices,
+                              host_image, 0, NULL, NULL);
+    assert(err == CL_SUCCESS);
+  }
+  for(i = 0; i < num_devices; i++) {
+    clFinish(cmd_queue[i]);
+  }
 
   // Now write the file
   write_bmp("output.bmp", width, height, host_image);
 
 	// Release OpenCL objects
 	clReleaseMemObject(image);
-	clReleaseCommandQueue(cmd_queue);
+  for(i = 0; i < num_devices; i++) {
+    clReleaseCommandQueue(cmd_queue[i]);
+  }
 	clReleaseContext(context);
   free(host_image);
 
